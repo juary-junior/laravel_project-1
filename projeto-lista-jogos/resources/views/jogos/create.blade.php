79df196c4726e74a59c39427fb19ba9192903bc3
juaryjunior <juaryjuniorr@gmail.com>
2024-05-16 09:25:31 -0300

    third commit

--- projeto-lista-jogos/resources/views/jogos/create.blade.php
+++ projeto-lista-jogos/resources/views/jogos/create.blade.php
@@ -2,29 +2,37 @@
 
 @section('content')
   
+
     <div class="container mt-5">
-        <h1>Crie um novo jogo</h1>
+    <div class="row">
+        <div class="col-sm-10">
+          <h1>Crie um Novo Jogo</h1>
+        </div>
+        <div class="col-sm-2">
+          <a href="{{ route('jogos-index') }}" class="btn btn-success">voltar</a>
+        </div>
+      </div>
         <hr>
         <form action="{{ route('jogos-store') }}" method="POST">
             @csrf
             <div class="form-group">
                 <label for="name">Nome:</label>
-                <input type="text" class="form-control" name="nome" placeholder="Digite um nome...">
+                <input type="text" class="form-control" name="nome" placeholder="Digite um nome..." required>
             </div>
             <br>
             <div class="form-group">
                 <label for="categoria">categoria:</label>
-                <input type="text" class="form-control" name="categoria" placeholder="Digite uma categoria para o jogo...">
+                <input type="text" class="form-control" name="categoria" placeholder="Digite uma categoria para o jogo..." required>
             </div>
             <br>
             <div class="form-group">
                 <label for="ano_criacao">Ano de criação:</label>
-                <input type="number" class="form-control" name="ano_criacao" placeholder="Ano de criação...">
+                <input type="number" class="form-control" name="ano_criacao" placeholder="Ano de criação..." required>
             </div>
             <br>
             <div class="form-group">
                 <label for="valor">Valor:</label>
-                <input type="number" class="form-control" name="valor" placeholder="Digite um valor para o jogo...">
+                <input type="number" class="form-control" name="valor" placeholder="Digite um valor para o jogo..." required>
             </div>
             <br>
             <div class="form-group">
